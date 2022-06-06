46b830dab962f993c3f58dae7d0b477d53fad29e
git-0r <rohitdec97@gmail.com>
2022-06-06 13:01:51 +0530

    fix geolocation api url

--- src/services/locationService.js
+++ src/services/locationService.js
@@ -1,6 +1,6 @@
 export const getLocationFromAPI = async ({ longitude, latitude }) => {
   const response = await fetch(
-    `http://api.positionstack.com/v1/reverse?access_key=${process.env.REACT_APP_GEOLOCATION_KEY}&query=${latitude},${longitude}&limit=1`
+    `https://api.positionstack.com/v1/reverse?access_key=${process.env.REACT_APP_GEOLOCATION_KEY}&query=${latitude},${longitude}&limit=1`
   );
   return await response.json();
 };
